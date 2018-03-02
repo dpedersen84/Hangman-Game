89d8cba97ef4d0f4568317fdb2651c2f893d89f0
Dan <dpedersen84@gmail.com>
2018-03-01 21:20:56 -0600

    some changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,23 @@
 var alphabet = ["a","b","c","d","e","f","g","h",
                 "i","j","k","l","m","n","o","p","q","r",
                 "s","t","u","v","w","x","y","z"];
-
+// Words To Guess
 var word = ["homer", "springfield", "kwik-e-mart", "eat my shorts", "bart", "lisa", "marge", "maggie"]
-
+// Starting Lives
 var lives = 10;
 
+
+document.getElementById("guess").value = "_";
+var answerArray = [];
+for (var i = 0; i <word.length; i++) {
+    answerArray[i] = "_";
+}
+
+// This section for when user guesses a letter
+
+var guessedLetters = document.getElementById("letters");
+
+document.onkeyup = function(event) {
+    guessedLetters.textContent = event.key;
+
+};
