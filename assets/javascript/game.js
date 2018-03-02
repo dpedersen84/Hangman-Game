1ee5e7dee1aa44a76d0c31af1191e40de8a9548b
Dan <dpedersen84@gmail.com>
2018-03-01 20:46:23 -0600

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Arrays
+
+var alphabet = ["a","b","c","d","e","f","g","h",
+                "i","j","k","l","m","n","o","p","q","r",
+                "s","t","u","v","w","x","y","z"];
+
+var word = ["homer", "springfield", "kwik-e-mart", "eat my shorts", "bart", "lisa", "marge", "maggie"]
+
+var lives = 10;
+
