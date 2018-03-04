70cf409492e44193af423c1f329c7d4dd3d042fc
Dan <dpedersen84@gmail.com>
2018-03-04 01:10:34 -0600

    functions and variables added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,61 @@
-// Arrays //
+//VARIABLES
 
-// Letters
+//Letters
+var letters = ["a","b","c","d","e","f","g","h",
+                 "i","j","k","l","m","n","o","p","q","r",
+                    "s","t","u","v","w","x","y","z"];
 
-var letter = ["a","b","c","d","e","f","g","h",
-                "i","j","k","l","m","n","o","p","q","r",
-                "s","t","u","v","w","x","y","z"];
+//Stats Default
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+var guessesLeft = 10;
+var guessedLetters = [];
+var guessThisWord = null;
 
-// Words To Guess
-
+// Words
 var wordArr = ["homer", "springfield", "kwik-e-mart", "eat my shorts", "bart", "lisa", "marge", "maggie"]
-var wordRandom = wordArr[Math.floor(Math.random() * wordArr.length)];
-document.getElementById(wordRandom, wordRandom < wordArr)
-console.log(wordRandom);
-
-// Starting Lives
-
-var lives = 10;
 
 
-// Functions //
+//FUNCTIONS
 
-document.getElementById("wordRandom");
-
-document.onkeyup = function(event) {
-
-    console.log(event.key);
+//Guesses Left
+function newGuessesLeft () {
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 };
+
+//Random Word Generated
+function newGuessThisWord () {
+    this.guessThisWord = this.wordArr[Math.floor(Math.random() * this.wordArr.length)];
+    console.log("Word To Guess: " + guessThisWord);
+};
+
+//Letters Guessed
+function newGuessedLetters () {
+    document.getElementById("letters").innerHTML = "Letters Guessed: " + guessedLetters.join(", ");
+};
+
+//Reset
+function reset () {
+    guessesLeft = 10;
+    guessedLetters = [];
+
+    //Call
+    newGuessesLeft ();
+    newGuessThisWord ();
+    newGuessedLetters ();
+}
+
+//Call
+newGuessThisWord ();
+newGuessesLeft ();
+
+//MAIN
+//Key Event
+document.onkeyup = function (event) {
+    guessesLeft--;
+    console.log("Guesses Left: " + guessesLeft);
+    console.log(event.key);
+
+    var letter = String.fromCharCode(event.which).toLowerCase();
+}
