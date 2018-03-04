18008e549b56231e15beaa14e5bcd47dd3bca55d
Dan <dpedersen84@gmail.com>
2018-03-04 16:32:05 -0600

    few js changes, sound added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,19 @@ var letters = ["a","b","c","d","e","f","g","h",
 //Stats Default
 var wins = 0;
 var losses = 0;
-var guesses = 10;
-var guessesLeft = 10;
+var guesses = 5;
+var guessesLeft = 5;
 var guessedLetters = [];
 var guessThisWord = null;
 
+//Sounds
+var audioElement1 = document.createElement("audio");
+    audioElement1.setAttribute("src", "./assets/sounds/Doh.wav");
+var audioElement2 = document.createElement("audio");
+    audioElement2.setAttribute("src", "./assets/sounds/WooHoo.wav");
+
 // Words
-var wordArr = ["homer", "springfield", "kwik-e-mart", "eat my shorts", "bart", "lisa", "marge", "maggie"]
+var wordArr = ["homer", "bart", "lisa", "marge", "maggie"]
 
 
 //FUNCTIONS
@@ -27,7 +33,7 @@ function newGuessesLeft () {
 //Random Word Generated
 function newGuessThisWord () {
     this.guessThisWord = this.wordArr[Math.floor(Math.random() * this.wordArr.length)];
-    console.log("Word To Guess: " + guessThisWord);
+    console.log("Word To Guess: " + guessThisWord);    
 };
 
 //Letters Guessed
@@ -37,7 +43,7 @@ function newGuessedLetters () {
 
 //Reset
 function reset () {
-    guessesLeft = 10;
+    guessesLeft = 5;
     guessedLetters = [];
 
     //Call
@@ -57,5 +63,34 @@ document.onkeyup = function (event) {
     console.log("Guesses Left: " + guessesLeft);
     console.log(event.key);
 
-    var letter = String.fromCharCode(event.which).toLowerCase();
+//Save Key to Variable    
+var letter = String.fromCharCode(event.which).toLowerCase();
+
+//Put Letter Variable into Guessed Letters Array
+    guessedLetters.push(letter); //Pushes Letter Variable to Guessed Letters Array
+    newGuessesLeft(); //Updates the number of Guesses Left
+    newGuessedLetters(); //Adds letter to Guessed Letters Array on DOM
+
+if (guessThisWord.includes(letter)) {
+    audioElement2.play(); //Sound on Win
+    wins++
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    alert("Winner!");
+    console.log("Winner!");
+    console.log("Wins: " + wins);
+    console.log("Losses: " + losses);
+    reset();
+
+}else if(guessesLeft == 0) {
+    audioElement1.play(); //Sound on Loss
+    losses++
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    alert("Game Over!");
+    console.log("Game Over!");
+    console.log("Wins: " + wins);
+    console.log("Losses: " + losses);
+    reset ();
 }
+
+
+};//END
